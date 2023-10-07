4120f68822e3fdc06b7781a4b27a6da4c9e4ec9b
Oleg Rybalko <rybalko.oleg.123@mail.ru>
2023-10-07 20:34:45 +0300

    feat: open post on click

--- labs/K33392/Рыбалко_Олег/lab1/src/components/Post/index.tsx
+++ labs/K33392/Рыбалко_Олег/lab1/src/components/Post/index.tsx
@@ -9,7 +9,6 @@ import {
   faComment as faCommentRegular,
 } from '@fortawesome/free-regular-svg-icons'
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
 
 export type PostType = {
   id: string
@@ -20,10 +19,10 @@ export type PostType = {
   commentsCount: number
 }
 
-export function Post(post: PostType) {
+export function Post({ post }: { post: PostType }) {
   const [isLiked, setLiked] = useState(false)
   const [commentsOpen, setCommentsOpen] = useState(false)
-  const navigate = useNavigate()
+  const [isOpen, setOpen] = useState(false)
 
   const like = () => {
     setLiked((liked) => !liked)
@@ -33,36 +32,30 @@ export function Post(post: PostType) {
     setCommentsOpen((isOpen) => !isOpen)
   }
 
-  const openPost = () => {
-    navigate(`/post/${post.id}`)
-  }
-
   return (
-    <>
-      <div className={`mt-3 ${styles.post}`}>
-        <div className={styles.body} onClick={openPost}>
-          <h1 className="h4">{post.title}</h1>
-          <p>{post.body.slice(0, 500)}...</p>
-        </div>
-
-        <div className={styles.footer}>
-          <button className={styles.likeButton} onClick={like}>
-            <FontAwesomeIcon
-              icon={isLiked ? faHeartSolid : faHeartRegular}
-              size="xl"
-            />
-            <p>{post.likesCount}</p>
-          </button>
-          <button className={`${styles.commentButton}`} onClick={comment}>
-            <FontAwesomeIcon
-              icon={commentsOpen ? faCommentSolid : faCommentRegular}
-              size="xl"
-            />
-            <p>{post.commentsCount}</p>
-          </button>
-        </div>
+    <div className={`mt-3 ${styles.post}`}>
+      <div className={styles.body} onClick={() => setOpen((open) => !open)}>
+        <h1 className="h4">{post.title}</h1>
+        <p>{isOpen ? post.body : `${post.body.slice(0, 500)}...`}</p>
       </div>
-    </>
+
+      <div className={styles.footer}>
+        <button className={styles.likeButton} onClick={like}>
+          <FontAwesomeIcon
+            icon={isLiked ? faHeartSolid : faHeartRegular}
+            size="xl"
+          />
+          <p>{post.likesCount}</p>
+        </button>
+        <button className={`${styles.commentButton}`} onClick={comment}>
+          <FontAwesomeIcon
+            icon={commentsOpen ? faCommentSolid : faCommentRegular}
+            size="xl"
+          />
+          <p>{post.commentsCount}</p>
+        </button>
+      </div>
+    </div>
   )
 }
 
--- labs/K33392/Рыбалко_Олег/lab1/src/layouts/Profile/index.tsx
+++ labs/K33392/Рыбалко_Олег/lab1/src/layouts/Profile/index.tsx
@@ -22,16 +22,22 @@ export function ProfileLayout() {
     })
   }, [username])
 
-  const posts: Array<PostType> = [
-    {
-      id: '1',
-      title: 'RE: LAST NIGHT',
-      body: 'Club culture around late 70s and 80s nightlife in NYC was a special, much talked about and written about thing. From the star studded midtown clubs like studio 54 and the first danceteria to the downtown clubs like Mudd + paradise garage. The figures, the music, the looks, the lack of regulation haha. I recognize NY wasn’t all lasers and disco lighting and that simultaneously, there was a lot of crime and poverty and that a huge part of club culture, the gay community, at that time were being wiped out by HIV + AIDS. Now in 2019, there’s a pill you can take every day that will at a better than 90% chance prevent you from contracting HIV. This pill was approved by the FDA in 2012. The pricing strategy behind it is malicious in my opinion and so it’s public perception is marred and rightfully so. But the fact remains that despite price being a very real barrier to this potentially life saving drug for some, the other very real barrier is awareness. I decided to name, what was otherwise going to be a night of lights and music inspired by an era of clubbing that I loved PrEP+ because while designing the club which is inside of an old glass factory basement in Queens (shoutout to The Basement that runs a very awesome techno night on Fridays after us) I started to imagine in an era where so many lives were lost and so much promise was lost forever along with them, what would it have been like if something, anything had existed that in all probability would’ve saved thousands and thousands of lives. I’m an artist, it’s core to my job to imagine realities that don’t necessarily exist and it’s a joy to. A couple days before we threw the party, I was discussing this subject with my team and one of the architects I work with thought that PrEP as a drug had reached ‘100% saturation’ so far as awareness. I thought he was dead wrong so I asked a friend (who I won’t name haha) if he knew what PrEP was and his response was ‘isn’t that some type of viagra or something’. My ex who I was with for several years didn’t know about it when we first met at a gay club in LA. Awareness isn’t always what we’d hope it would be. But anyway, I’m ranting. I’m happy that folks are talking about the subject in the first place. Thank you to everyone who came out and danced with us last night. Y’all were beautiful and the energy was right! Thank you Bouffant Bouffant, Sango, Justice and Sherelle for your sets last night they were soo good man. Oh one more thing, I saw someone say that this was a PR stunt etc etc, pshhh bitch pls come get a drink next time and I’ll put several barstools out so you can have as many seats as you need. All my love everybody really. Stay safe.',
-      authorUsername: 'rybalkooleg',
-      likesCount: 100,
-      commentsCount: 100,
-    },
-  ]
+  const [posts, setPosts] = useState<PostType[]>([])
+
+  // TODO: fetch posts
+  useEffect(() => {
+    setPosts([
+      ...posts,
+      {
+        id: '1',
+        title: 'RE: LAST NIGHT',
+        body: 'Club culture around late 70s and 80s nightlife in NYC was a special, much talked about and written about thing. From the star studded midtown clubs like studio 54 and the first danceteria to the downtown clubs like Mudd + paradise garage. The figures, the music, the looks, the lack of regulation haha. I recognize NY wasn’t all lasers and disco lighting and that simultaneously, there was a lot of crime and poverty and that a huge part of club culture, the gay community, at that time were being wiped out by HIV + AIDS. Now in 2019, there’s a pill you can take every day that will at a better than 90% chance prevent you from contracting HIV. This pill was approved by the FDA in 2012. The pricing strategy behind it is malicious in my opinion and so it’s public perception is marred and rightfully so. But the fact remains that despite price being a very real barrier to this potentially life saving drug for some, the other very real barrier is awareness. I decided to name, what was otherwise going to be a night of lights and music inspired by an era of clubbing that I loved PrEP+ because while designing the club which is inside of an old glass factory basement in Queens (shoutout to The Basement that runs a very awesome techno night on Fridays after us) I started to imagine in an era where so many lives were lost and so much promise was lost forever along with them, what would it have been like if something, anything had existed that in all probability would’ve saved thousands and thousands of lives. I’m an artist, it’s core to my job to imagine realities that don’t necessarily exist and it’s a joy to. A couple days before we threw the party, I was discussing this subject with my team and one of the architects I work with thought that PrEP as a drug had reached ‘100% saturation’ so far as awareness. I thought he was dead wrong so I asked a friend (who I won’t name haha) if he knew what PrEP was and his response was ‘isn’t that some type of viagra or something’. My ex who I was with for several years didn’t know about it when we first met at a gay club in LA. Awareness isn’t always what we’d hope it would be. But anyway, I’m ranting. I’m happy that folks are talking about the subject in the first place. Thank you to everyone who came out and danced with us last night. Y’all were beautiful and the energy was right! Thank you Bouffant Bouffant, Sango, Justice and Sherelle for your sets last night they were soo good man. Oh one more thing, I saw someone say that this was a PR stunt etc etc, pshhh bitch pls come get a drink next time and I’ll put several barstools out so you can have as many seats as you need. All my love everybody really. Stay safe.',
+        authorUsername: 'rybalkooleg',
+        likesCount: 100,
+        commentsCount: 100,
+      },
+    ])
+  }, [])
 
   return (
     <div className="container">
@@ -48,14 +54,7 @@ export function ProfileLayout() {
         <h1 className="h3">{t('postsHeader')}</h1>
         <hr />
         {posts.map((post) => (
-          <Post
-            id={post.id}
-            title={post.title}
-            body={post.body}
-            authorUsername={post.authorUsername}
-            likesCount={post.likesCount}
-            commentsCount={post.commentsCount}
-          />
+          <Post post={post} />
         ))}
       </div>
     </div>
