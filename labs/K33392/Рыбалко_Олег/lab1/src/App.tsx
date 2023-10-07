52ac7093bb86df65c53d4ba9856d596062708a2d
Oleg Rybalko <rybalko.oleg.123@mail.ru>
2023-10-07 15:23:39 +0300

    feat: added new route for signin

--- labs/K33392/Рыбалко_Олег/lab1/src/App.tsx
+++ labs/K33392/Рыбалко_Олег/lab1/src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { SignUpLayout } from './layout/SignUp'
+import { SignUpLayout } from './layouts/SignUp'
+import { SignInLayout } from './layouts/SignIn'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/signin" element={<SignInLayout />} />
         <Route path="/signup" element={<SignUpLayout />} />
       </Routes>
     </BrowserRouter>
