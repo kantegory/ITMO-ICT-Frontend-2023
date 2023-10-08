9f5bb492fd15ddb61a3b12222fafe1cec959604d
Oleg Rybalko <rybalko.oleg.123@mail.ru>
2023-10-08 21:59:44 +0300

    feat: added one more text

--- labs/K33392/Рыбалко_Олег/lab1/src/layouts/Profile/index.tsx
+++ labs/K33392/Рыбалко_Олег/lab1/src/layouts/Profile/index.tsx
@@ -60,6 +60,17 @@ export function ProfileLayout() {
         authorUsername: 'rybalkooleg',
         likesCount: 100,
       },
+      {
+        id: '2',
+        title: 'MERCURY',
+        body: `Mercury—the smallest planet in our solar system and nearest to the Sun—is only slightly larger than Earth's Moon. Its surface is covered in tens of thousands of impact craters.
+
+        From the surface of Mercury, the Sun would appear more than three times as large as it does when viewed from Earth, and the sunlight would be as much as 11 times brighter.
+        
+        Despite its proximity to the Sun, Mercury is not the hottest planet in our solar system— that title belongs to nearby Venus, thanks to its dense atmosphere. But Mercury is the fastest planet, zipping around the Sun every 88 Earth days. Mercury is appropriately named for the swiftest of the ancient Roman gods.`,
+        authorUsername: 'rybalkooleg',
+        likesCount: 158,
+      },
     ])
   }, [])
 
