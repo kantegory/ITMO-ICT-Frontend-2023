4d743957696a7a9e361610dba99879f8c62c5592
Nikita <tyumin000@gmail.com>
2023-10-11 17:31:29 +0300

    added search page

--- labs/K33402/Тюмин Никита/lab1/src/router/routes.js
+++ labs/K33402/Тюмин Никита/lab1/src/router/routes.js
@@ -1,8 +1,12 @@
 import HomePage from "../views/HomePage.vue";
 import ProfilePage from "../views/ProfilePage.vue";
+import SearchPage from "../views/SearchPage.vue";
+
 import LoginPage from "../views/auth/LoginPage.vue";
 import RegisterPage from "../views/auth/RegisterPage.vue";
+
 import NotFoundPage from "../views/NotFoundPage.vue";
+
 import SecureComponent from "../components/SecureComponent.vue";
 import PublicComponent from "../components/PublicComponent.vue";
 
@@ -21,6 +25,11 @@ export default [
                 path: '/profile',
                 name: 'profile',
                 component: ProfilePage
+            },
+            {
+                path: '/search',
+                name: 'search',
+                component: SearchPage
             },
         ],
     },
