b875fcef9d185df26c7168815a02a0a0bc44cc30
sokolovskayaa <ssokolovskayaaa@gmail.com>
2023-10-20 07:50:44 +0300

    fix: минорные изменения

--- labs/K33392/Pronina_Sokolovskaya/script.js
+++ labs/K33392/Pronina_Sokolovskaya/script.js
@@ -48,13 +48,13 @@ function filterList() {
     let displayedCount = 0;
     currentQuests.forEach((quest) => {
       let questDisplay = getComputedStyle(quest, null).display;
-      if (questDisplay != 'none') {
+      if (questDisplay !== 'none') {
         displayedCount = 1;
         item.style.display = '';
         return;
       }
     })
-    if (displayedCount == 0) {
+    if (displayedCount === 0) {
       item.style.display = 'none';
     }
   })
@@ -103,13 +103,13 @@ function displayFiltered() {
     let displayedCount = 0;
     currentQuests.forEach((quest) => {
       let questDisplay = getComputedStyle(quest, null).display;
-      if (questDisplay != 'none') {
+      if (questDisplay !== 'none') {
         displayedCount = 1;
         item.style.display = '';
         return;
       }
     })
-    if (displayedCount == 0) {
+    if (displayedCount === 0) {
       item.style.display = 'none';
     }
   })
