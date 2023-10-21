24b009c272af21384f67c622d624d9544e3e5435
Alexey Korotin <sx12fx@gmail.com>
2023-10-21 23:54:09 +0300

    feat: add login functionality

--- labs/K33392/Коротин Алексей/lab2/res/scripts/signup.js
+++ labs/K33392/Коротин Алексей/lab2/res/scripts/signup.js
@@ -1,24 +1,8 @@
 import axios from 'https://cdn.jsdelivr.net/npm/axios@1.5.1/+esm';
+import { alert, checkAuth } from './auth.js';
 
-const alertPlaceholder = document.getElementById("alert-placeholder");
 
-const alert = (message) => {
-    const wrapper = document.createElement('div');
-    wrapper.innerHTML = [
-        `<div class="alert alert-warning alert-dismissible" role="alert">`,
-        `   <div>${message}</div>`,
-        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-        '</div>'
-    ].join('');
-    
-    alertPlaceholder.append(wrapper);
-}
 
-const checkAuth = () => {
-    if (window.localStorage.accessToken) {
-        window.location.href = "/pages/collection.html";
-    }
-}
 
 const signUp = async (event) => {    
     await event.preventDefault();
@@ -32,13 +16,13 @@ const signUp = async (event) => {
     }
     
     if (signUpData.password !== signUpData.passwordRepeat) {
-        alert("Passwords does not match");
+        alert("Passwords does not match", "alert-placeholder");
         return;
     }
     delete signUpData.passwordRepeat;
 
     if (signUpData.password.length < 5) {
-        alert("Password is too short. Try again");
+        alert("Password is too short. Try again", "alert-placeholder");
         return;
     }
 
@@ -49,7 +33,7 @@ const signUp = async (event) => {
             checkAuth();
         })
         .catch((reason) => {
-            alert("Email is already taken. Try another");
+            alert("Email is already taken. Try another", "alert-placeholder");
         });
 
 };
