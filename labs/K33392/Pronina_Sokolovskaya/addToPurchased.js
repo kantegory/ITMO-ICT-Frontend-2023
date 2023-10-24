2afcaa7d5b8232eae7bf4f0a8aba7a4e6d1c075d
marypronina <90632358+marypronina@users.noreply.github.com>
2023-10-24 14:49:09 +0300

    feat: test api

---
+++ labs/K33392/Pronina_Sokolovskaya/addToPurchased.js
@@ -0,0 +1,20 @@
+function addToPurchased(el) {
+    let id = el.parentNode.parentNode.id;
+    fetch("http://127.0.0.1:8090/api/collections/quest_card/records").then(Response => Response.json()).then(data => {
+        let quests = data.items;
+        let quest_to_buy = quests.find(quest => quest.id === id);
+        // console.log(quest_to_buy);
+        if (quest_to_buy) {
+            fetch("http://127.0.0.1:8090/api/collections/purchased_quest/records", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({quest_id: quest_to_buy.id})
+            });
+        }
+        else {
+            console.log("not found");
+        }
+    })
+}
