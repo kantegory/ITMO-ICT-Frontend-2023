836dc55be367e070dcbfc3f8215ea6e6b2523d1a
Oleg Rybalko <rybalko.oleg.123@mail.ru>
2023-10-25 18:26:25 +0300

    feat: working likes button

--- labs/K33392/Рыбалко_Олег/lab1/src/components/Post/index.tsx
+++ labs/K33392/Рыбалко_Олег/lab1/src/components/Post/index.tsx
@@ -5,12 +5,18 @@ import {
   faTrashCan,
 } from '@fortawesome/free-solid-svg-icons'
 import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons'
-import { useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { PostType } from '@/types'
+import { useSelector } from 'react-redux'
+import { RootState } from '@/store'
+import { AuthState } from '@/store/slices/auth'
+import { pb } from '@/constants'
+import { RecordModel } from 'pocketbase'
 
 export function Post({
   className,
   post,
+  posts,
   onDelete,
   showDeleteButton,
 }: {
@@ -18,26 +24,67 @@ export function Post({
   post: PostType
   onDelete?: (post: PostType) => void
   showDeleteButton: boolean
+  posts: PostType[]
 }) {
   const [isLiked, setLiked] = useState(false)
+  const [likedRecord, setLikedRecord] = useState<RecordModel | undefined>()
+  const authStore = useSelector<RootState>((state) => state.auth) as AuthState
   const [isOpen, setOpen] = useState(false)
   const textPreviewLength = 500
 
-  const like = () => {
-    setLiked((liked) => !liked)
-  }
+  useEffect(() => {
+    pb.collection('likes')
+      .getFirstListItem(`(post='${post.id}' && user='${authStore.id}')`)
+      .then((record) => {
+        setLiked(true)
+        setLikedRecord(record)
+      })
+      .catch(() => setLiked(false))
+  }, [authStore, setLiked, post])
+
+  const [likesCount, setLikesCount] = useState(0)
+
+  useEffect(() => {
+    pb.collection('likes')
+      .getFullList({ filter: `post="${post.id}"` })
+      .then((records) => {
+        setLikesCount(records.length)
+      })
+  }, [posts, post])
+
+  const like = useCallback(() => {
+    if (!isLiked) {
+      pb.collection('likes')
+        .create({ post: post.id, user: authStore.id })
+        .then((record) => {
+          setLikesCount((likes) => likes + 1)
+          setLiked(true)
+          setLikedRecord(record)
+        })
+    } else if (likedRecord !== undefined) {
+      pb.collection('likes')
+        .delete(likedRecord.id)
+        .then(() => {
+          setLikesCount((likes) => likes - 1)
+          setLiked(false)
+          setLikedRecord(undefined)
+        })
+    }
+  }, [isLiked, likedRecord, authStore, post])
 
   return (
     <div className={`mt-3 ${className}`}>
       <div className={styles.body} onClick={() => setOpen((open) => !open)}>
         <h1 className="h4">{post.title}</h1>
-        <p>
-          {isOpen
-            ? post.body
-            : `${post.body.slice(0, textPreviewLength)}${
-                post.body.length > textPreviewLength ? '...' : ''
-              }`}
-        </p>
+        <p
+          dangerouslySetInnerHTML={{
+            __html: isOpen
+              ? post.body
+              : `${post.body.slice(0, textPreviewLength)}${
+                  post.body.length > textPreviewLength ? '...' : ''
+                }`,
+          }}
+        ></p>
       </div>
 
       <div className={styles.footer}>
@@ -55,7 +102,7 @@ export function Post({
             icon={isLiked ? faHeartSolid : faHeartRegular}
             size="xl"
           />
-          <p>{post.likesCount}</p>
+          <p>{likesCount}</p>
         </button>
       </div>
     </div>
