55ecff5bc899f74e07695753785ebb1e8cb20652
Oleg Rybalko <rybalko.oleg.123@mail.ru>
2023-10-29 21:24:40 +0300

    fix: removed lab from here

--- homeworks/K33392/Рыбалко_Олег/hw2/src/layouts/NotFound/index.tsx
+++
@@ -1,8 +0,0 @@
-import { useTranslation } from 'react-i18next'
-import styles from './NotFound.module.scss'
-
-export function PageNotFound() {
-  const { t } = useTranslation('global')
-  return <h1 className={`h1 ${styles.notFoundHeader}`}>{t('notFound')}</h1>
-}
-
--- homeworks/K33392/Рыбалко_Олег/hw2/src/layouts/Profile/Profile.module.scss.d.ts
+++
@@ -1,6 +0,0 @@
-export const profileImage: string
-export const profileInfo: string
-export const postsHeader: string
-export const container: string
-export const post: string
-export const bioBlock: string
