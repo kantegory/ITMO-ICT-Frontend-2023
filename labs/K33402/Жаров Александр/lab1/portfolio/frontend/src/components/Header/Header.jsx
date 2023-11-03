dc8493752094f3ccc120f53331ee8a5630b1e81a
SashaZharov <zhara190@gmail.com>
2023-11-03 18:54:44 +0300

    Add report and code for hw4

--- labs/K33402/Жаров Александр/lab1/portfolio/frontend/src/components/Header/Header.jsx
+++ labs/K33402/Жаров Александр/lab1/portfolio/frontend/src/components/Header/Header.jsx
@@ -3,12 +3,10 @@ import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import icon from "../../img/main-icon.svg";
-import sun from "../../img/sun.svg";
-import moon from "../../img/moon-stars.svg";
 import { Link } from "react-router-dom";
 import SearchCell from "../SearchCell";
 import useTheme from "../../hooks/useTheme";
+import { Icon } from "../Icon";
 
 function Header() {
   const [searchQuery, setSearchQuery] = useState("");
@@ -24,11 +22,7 @@ function Header() {
       <Navbar expand="md" className="header" data-bs-theme="dark" sticky="top">
         <Container>
           <Navbar.Brand>
-            <img
-              src={icon}
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
+            <Icon id="main" height={40} width={40} />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
@@ -43,9 +37,9 @@ function Header() {
             </Nav>
             <button className="theme-button" onClick={ChangeTheme}>
               {theme === "dark" ? (
-                <img src={moon} alt="" height={24} width={24} />
+                <Icon id="moon" height={24} width={24} />
               ) : (
-                <img src={sun} alt="" height={24} width={24} />
+                <Icon id="sun" height={24} width={24} />
               )}
             </button>
             <div className="md-block">
