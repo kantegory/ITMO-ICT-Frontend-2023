f802042d26949f1302bcbd554efb1ef09fd28ce2
rashevskiivv <slava121231@mail.ru>
2023-11-10 18:51:58 +0300

    modify gitignore and rename file

--- labs/K33402/Рашевский Вячеслав/lab1/js/auth.js
+++ labs/K33402/Рашевский Вячеслав/lab1/js/auth.js
@@ -1,4 +1,4 @@
-function signup() {
+function auth() {
     // Javascript reGex for Email Validation.
     const regEmail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;
     const regPhone=/^\d{10}$/;   // Javascript reGex for Phone Number validation.
