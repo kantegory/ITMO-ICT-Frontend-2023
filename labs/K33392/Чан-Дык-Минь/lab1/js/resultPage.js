6fa1b0c128420a3905ff8fc31c6df58f24358c73
Mesh296 <minhhan181@gmail.com>
2023-11-12 19:22:44 +0300

    feat: added hw3 and hw4

--- labs/K33392/Чан-Дык-Минь/lab1/js/resultPage.js
+++ labs/K33392/Чан-Дык-Минь/lab1/js/resultPage.js
@@ -5,6 +5,7 @@ const BMItext = document.getElementById("BMItext");
 const maintainCalo = document.getElementById("maintainCalo");
 const cuttingCalo = document.getElementById("cuttingCalo");
 const bulkingCalo = document.getElementById("bulkingCalo");
+const BMIClass = document.getElementById("BMIClass");
 
 BMR.innerHTML = localStorage.getItem("BMR");
 BMI.innerHTML = localStorage.getItem("BMI");
@@ -14,3 +15,19 @@ BMItext.innerHTML = BMI.innerHTML;
 maintainCalo.innerHTML = BMR.innerHTML;
 cuttingCalo.innerHTML = parseInt(BMR.innerHTML) - 500;
 bulkingCalo.innerHTML = parseInt(BMR.innerHTML) + 500;
+
+let intBMI = parseInt(BMI.innerHTML);
+if(intBMI <= 18.5){
+    BMIClass.innerHTML = "Underweight";
+}
+else if (intBMI >= 18.5 && intBMI  <= 24.9){
+    BMIClass.innerHTML = "Normal Weight";
+}
+else if (intBMI >= 25 && intBMI  <= 29.9){
+    BMIClass.innerHTML = "Overweight";
+}
+else if (intBMI >= 30){
+    BMIClass.innerHTML = "Obesity";
+}
+
+
