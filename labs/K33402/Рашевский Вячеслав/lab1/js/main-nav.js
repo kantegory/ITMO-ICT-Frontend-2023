1da8b792d10bcbbc86223972a1d7378f8c0dd24d
rashevskiivv <slava121231@mail.ru>
2023-11-28 16:27:28 +0300

    enable cdn + add code to report

--- labs/K33402/Рашевский Вячеслав/lab1/js/main-nav.js
+++
@@ -1,9 +0,0 @@
-void async function () {
-    //get the imported document in templates:
-    const templates = document.createElement("template");
-    templates.innerHTML = await (await fetch("templates.html")).text()
-
-    //fetch main-nav template:
-    const element = templates.content.querySelector("#main-nav");
-    document.body.appendChild(element.content);
-}()
