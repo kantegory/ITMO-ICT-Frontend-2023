f29714034f2febc13ef5952f0bca72204127987f
rashevskiivv <slava121231@mail.ru>
2023-12-02 14:06:03 +0300

    lab2 - working on api.js - filling services

--- labs/K33402/Рашевский Вячеслав/lab1/js/api.js
+++ labs/K33402/Рашевский Вячеслав/lab1/js/api.js
@@ -26,7 +26,6 @@ async function fetchAbout() {
 
 const aboutTitle = document.getElementById("aboutTitle");
 const aboutDesc = document.getElementById("aboutDesc");
-
 if (aboutTitle && aboutDesc) {
     fetchAbout().then(about => {
         console.log(about);
@@ -53,3 +52,55 @@ if (ulUseCases) {
     });
 }
 
+const mainContainer = document.getElementById('cards');
+if (mainContainer) {
+    const list = document.createDocumentFragment();
+    fetchServices().then((data) => {
+        console.log(data);
+        data.map(function (service) {
+            const div1 = document.createElement('div');
+            div1.className = "col-12 col-xl-4 col-md-6 my-3";
+
+            const divCard = document.createElement('div');
+            divCard.className = "card";
+
+            const img = document.createElement('img');
+            img.src = service.src;
+            img.alt = service.alt;
+            img.className = "card-img-top object-fit-contain";
+            img.width = 500;
+            img.height = 500;
+
+            const divBody = document.createElement('div');
+            divBody.className = "card-body";
+
+            const h5Title = document.createElement('h5');
+            h5Title.className = "card-title";
+            h5Title.innerHTML = `${service.title}`;
+
+            const pText = document.createElement('p');
+            pText.className = "card-text";
+            pText.innerHTML = `${service.text}`;
+
+            const a = document.createElement('a');
+            a.className = "btn btn-primary";
+            a.href = `${service.href}`;
+            a.innerHTML = `Check ${service.title} service`;
+
+            divBody.appendChild(h5Title);
+            divBody.appendChild(pText);
+            divBody.appendChild(a);
+
+            divCard.appendChild(img);
+            divCard.appendChild(divBody);
+
+            div1.appendChild(divCard);
+
+            console.log(div1);
+
+            list.appendChild(div1);
+        });
+    }).finally(() => {
+        mainContainer.appendChild(list);
+    });
+}
