c72fc0d47afde54426de10db37c1eb29876891c7
elizavetaplastun <lizaplastun@gmail.com>
2023-12-08 16:35:21 +0300

    add more playlists logic

--- labs/K33402/Пластун Елизавета /lr1/www/js/music.js
+++ labs/K33402/Пластун Елизавета /lr1/www/js/music.js
@@ -4,159 +4,238 @@ let prevBtn = document.getElementById("prev");   // Берём кнопку пе
 let nextBtn = document.getElementById("next");   // Берём кнопку переключение следующего трека
 let musicName = document.getElementById("current-song-name");
 let musicArtist = document.getElementById("current-autor");
-let musicImg = document.getElementById("current-img");
 let wrapper = document.querySelector(".wrapper");
 let progressArea = document.querySelector(".progress-area");
 let progressBar = document.querySelector(".progress-bar");
 let volumeScale = document.getElementById("volume");
-let allMusic = {};
-// В браузере
-fetch('../json/musicData.json')
-    .then(response => response.json())
-    .then(data => {allMusic = data;
-    })
-    .catch(error => console.error('Ошибка загрузки файла:', error));
+const songContainerAll = document.getElementById('songContainerAll');
+const songContainerMyMusic = document.getElementById('songContainerMyMusic');
+const songContainerMyPlaylists = document.getElementById('songContainerMyPlaylists');
+const SERVER_URL = 'http://localhost:3000'
 
+async function getDataAllMusic() {
+    try {
+        const response = await fetch(`${SERVER_URL}/all_music`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return Array.isArray(data) ? data : [];
+    } catch (error) {
+        console.error('Произошла ошибка при получении данных:', error);
+        return [];
+    }
+}
+async function getDataMyMusic() {
+    try {
+        const response = await fetch(`${SERVER_URL}/my_music`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return Array.isArray(data) ? data : [];
+    } catch (error) {
+        console.error('Произошла ошибка при получении данных:', error);
+        return [];
+    }
+}
+async function getDataMyPlaylists() {
+    try {
+        const response = await fetch(`${SERVER_URL}/playlists`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return Array.isArray(data) ? data : [];
+    } catch (error) {
+        console.error('Произошла ошибка при получении данных:', error);
+        return [];
+    }
+}
 
-let musicIndex = Math.floor((Math.random() * allMusic.length) + 1);
-isMusicPaused = true;
+window.addEventListener("load", () => {
+    getDataAllMusic().then(data => {
+        let allMusic = data;
+        let musicIndex = Math.floor((Math.random() * allMusic.length) + 1);
+        createHtmlSongList(allMusic,songContainerAll);
+        loadMusic(musicIndex,allMusic);
+        prevBtn.addEventListener("click", () => {
+            musicIndex = prevMusic(musicIndex, allMusic);
+        });
+        nextBtn.addEventListener("click", () => {
+            musicIndex = nextMusic(musicIndex, allMusic);
+        });
 
-window.addEventListener("load", ()=>{
-    loadMusic(musicIndex);
-    // playingSong();
+    }).catch(err => {
+        console.error('Произошла ошибка:', err);
+    });
+    getDataMyMusic().then(data => {
+        createHtmlSongList(data,songContainerMyMusic);
+    }).catch(err => {
+        console.error('Произошла ошибка:', err);
+    });
+    getDataMyPlaylists().then(data => {
+        createHtmlPlayList(data,songContainerMyPlaylists);
+    }).catch(err => {
+        console.error('Произошла ошибка:', err);
+    });
 });
 
-function loadMusic(indexNumb){
+
+function loadMusic(indexNumb,allMusic) {
     musicName.innerText = allMusic[indexNumb - 1].name;
     musicArtist.innerText = allMusic[indexNumb - 1].artist;
-    musicImg.src = allMusic[indexNumb - 1].img;
     mainAudio.src = `../../src/music/${allMusic[indexNumb - 1].filename}`;
 }
-// function loadMusic(indexNumb){
-//     musicName.innerText = allMusic[indexNumb - 1].name;
-//     musicArtist.innerText = allMusic[indexNumb - 1].artist;
-//     musicImg.src = `../../src/img/${allMusic[indexNumb - 1].src}.png`;
-//     mainAudio.src = `../../src/music/${allMusic[indexNumb - 1].src}.mp3`;
-// }
 
-//play music function
-function playMusic(){
+
+function playMusic() {
     wrapper.classList.add("paused");
     playPauseBtn.innerText = "pause";
     mainAudio.play();
 }
 
-//pause music function
-function pauseMusic(){
+function pauseMusic() {
     wrapper.classList.remove("paused");
     playPauseBtn.innerText = "play_arrow";
     mainAudio.pause();
 }
 
-//prev music function
-function prevMusic(){
-    musicIndex--; //decrement of musicIndex by 1
-    //if musicIndex is less than 1 then musicIndex will be the array length so the last music play
+function prevMusic(musicIndex, allMusic) {
+    musicIndex--;
     musicIndex < 1 ? musicIndex = allMusic.length : musicIndex = musicIndex;
-    loadMusic(musicIndex);
+    loadMusic(musicIndex,allMusic);
     playMusic();
-    // playingSong();
+    return musicIndex;
 }
 
-//next music function
-function nextMusic(){
-    musicIndex++; //increment of musicIndex by 1
-    //if musicIndex is greater than array length then musicIndex will be 1 so the first music play
+function nextMusic(musicIndex, allMusic) {
+    musicIndex++;
     musicIndex > allMusic.length ? musicIndex = 1 : musicIndex = musicIndex;
-    loadMusic(musicIndex);
+    loadMusic(musicIndex,allMusic);
     playMusic();
-    playingSong();
+    return musicIndex;
 }
 
-// play or pause button event
-playPauseBtn.addEventListener("click", ()=>{
+playPauseBtn.addEventListener("click", () => {
     const isMusicPlay = wrapper.classList.contains("paused");
-    //if isPlayMusic is true then call pauseMusic else call playMusic
     isMusicPlay ? pauseMusic() : playMusic();
-    // playingSong();
 });
 
 
-//prev music button event
-prevBtn.addEventListener("click", ()=>{
-    prevMusic();
-});
-
-//next music button event
-nextBtn.addEventListener("click", ()=>{
-    nextMusic();
-});
-mainAudio.addEventListener("timeupdate", (e)=>{
-    const currentTime = e.target.currentTime; //getting playing song currentTime
-    const duration = e.target.duration; //getting playing song total duration
+mainAudio.addEventListener("timeupdate", (e) => {
+    const currentTime = e.target.currentTime;
+    const duration = e.target.duration;
     let progressWidth = (currentTime / duration) * 100;
     progressBar.style.width = `${progressWidth}%`;
     let musicCurrentTime = wrapper.querySelector(".current-time"),
         musicDuartion = wrapper.querySelector(".max-duration");
-    mainAudio.addEventListener("loadeddata", ()=>{
-        // update song total duration
+    mainAudio.addEventListener("loadeddata", () => {
+
         let mainAdDuration = mainAudio.duration;
         let totalMin = Math.floor(mainAdDuration / 60);
         let totalSec = Math.floor(mainAdDuration % 60);
-        if(totalSec < 10){ //if sec is less than 10 then add 0 before it
+        if (totalSec < 10) {
             totalSec = `0${totalSec}`;
         }
         musicDuartion.innerText = `${totalMin}:${totalSec}`;
     });
-    // update playing song current time
+
     let currentMin = Math.floor(currentTime / 60);
     let currentSec = Math.floor(currentTime % 60);
-    if(currentSec < 10){ //if sec is less than 10 then add 0 before it
+    if (currentSec < 10) {
         currentSec = `0${currentSec}`;
     }
     musicCurrentTime.innerText = `${currentMin}:${currentSec}`;
 });
-// update playing song currentTime on according to the progress bar width
-progressArea.addEventListener("click", (e)=>{
-    let progressWidth = progressArea.clientWidth; //getting width of progress bar
-    let clickedOffsetX = e.offsetX; //getting offset x value
-    let songDuration = mainAudio.duration; //getting song total duration
 
+progressArea.addEventListener("click", (e) => {
+    let progressWidth = progressArea.clientWidth;
+    let clickedOffsetX = e.offsetX;
+    let songDuration = mainAudio.duration;
     mainAudio.currentTime = (clickedOffsetX / progressWidth) * songDuration;
-    playMusic(); //calling playMusic function
-    // playingSong();
+    playMusic();
 });
 
-
-mainAudio.addEventListener("ended", ()=>{
-    // we'll do according to the icon means if user has set icon to
-    // loop song then we'll repeat the current song and will do accordingly
-    let getText = repeatBtn.innerText; //getting this tag innerText
-    switch(getText){
-        case "repeat":
-            nextMusic(); //calling nextMusic function
-            break;
-        case "repeat_one":
-            mainAudio.currentTime = 0; //setting audio current time to 0
-            loadMusic(musicIndex); //calling loadMusic function with argument, in the argument there is a index of current song
-            playMusic(); //calling playMusic function
-            break;
-        case "shuffle":
-            let randIndex = Math.floor((Math.random() * allMusic.length) + 1); //genereting random index/numb with max range of array length
-            do{
-                randIndex = Math.floor((Math.random() * allMusic.length) + 1);
-            }
-            while(musicIndex === randIndex); //this loop run until the next random number won't be the same of current musicIndex
-            musicIndex = randIndex; //passing randomIndex to musicIndex
-            loadMusic(musicIndex);
-            playMusic();
-            // playingSong();
-            break;
-    }
-});
-
-function ChangeVolume() { //Меняем громкость
-    var volume=  volumeScale.value / 100;
+function ChangeVolume() {
+    var volume = volumeScale.value / 100;
     mainAudio.volume = parseFloat(volume.toString());
 }
-volumeScale.addEventListener('change',ChangeVolume);
+
+function createHtmlSongList(allMusic, parent) {
+    allMusic.forEach((song) => {
+        const songDiv = document.createElement('div');
+        songDiv.classList.add('song-container-main');
+        const songPhotoDiv = document.createElement('div');
+        songPhotoDiv.classList.add('container-song-photo');
+        const songInfoDiv = document.createElement('div');
+        songInfoDiv.classList.add('song-container');
+        const songImage = document.createElement('img');
+        songImage.classList.add('img-thumbnail', 'resize')
+        songImage.src = `../../src/img/1.png`;
+        const songName = document.createElement('div');
+        songName.classList.add('nav-link', 'black');
+        songName.textContent = song.name;
+        const songArtist = document.createElement('div');
+        songArtist.classList.add('nav-link', 'grey');
+        songArtist.textContent = song.artist;
+
+        songInfoDiv.appendChild(songName);
+        songInfoDiv.appendChild(songArtist);
+        songPhotoDiv.appendChild(songImage);
+        songPhotoDiv.appendChild(songInfoDiv);
+        songDiv.appendChild(songPhotoDiv);
+        parent.appendChild(songDiv);
+    });
+}
+
+
+function createHtmlPlayList(playlists, parent) {
+    playlists.forEach(playlist => {
+        const playlistDiv = document.createElement('div');
+        playlistDiv.classList.add('accordion-item');
+        const playlistH2 = document.createElement('h2');
+        playlistH2.classList.add('accordion-header');
+        const playlistDivcollapse = document.createElement('div');
+        playlistDivcollapse.id = `playlist_${playlist.id}`;
+        playlistDivcollapse.classList.add('accordion-collapse', 'collapse');
+        const PlaylistButton = document.createElement('button');
+        PlaylistButton.classList.add('accordion-button', 'collapsed');
+        PlaylistButton.setAttribute('type', 'button');
+        PlaylistButton.setAttribute('data-bs-toggle', 'collapse');
+        PlaylistButton.setAttribute('data-bs-target', `#playlist_${playlist.id}`);
+        PlaylistButton.setAttribute('aria-expanded', 'false');
+        PlaylistButton.setAttribute('aria-controls', `playlist_${playlist.id}`);
+        const musiclist = document.createElement('div');
+        musiclist.classList.add('accordion-body');
+
+        const songDivi = document.createElement('div');
+        songDivi.classList.add('song-container-main');
+        const songPhotoDivi = document.createElement('div');
+        songPhotoDivi.classList.add('container-song-photo');
+        const songInfoDivi = document.createElement('div');
+        songInfoDivi.classList.add('song-container');
+        const songImagei = document.createElement('img');
+        songImagei.classList.add('img-thumbnail', 'resize');
+        songImagei.src = `../../src/img/1.png`;
+        const songNamei = document.createElement('div');
+        songNamei.classList.add('nav-link', 'black');
+        songNamei.textContent = playlist.name;
+        console.log(playlist.list)
+        createHtmlSongList(playlist.list, musiclist);
+
+        songInfoDivi.appendChild(songNamei);
+        songPhotoDivi.appendChild(songImagei);
+        songPhotoDivi.appendChild(songInfoDivi);
+        songDivi.appendChild(songPhotoDivi);
+        PlaylistButton.appendChild(songDivi);
+        playlistH2.appendChild(PlaylistButton);
+        playlistDiv.appendChild(playlistH2);
+        playlistDiv.appendChild(playlistDivcollapse);
+        playlistDivcollapse.appendChild(musiclist)
+        parent.appendChild(playlistDiv);
+    });
+}
+
+
+volumeScale.addEventListener('change', ChangeVolume);
