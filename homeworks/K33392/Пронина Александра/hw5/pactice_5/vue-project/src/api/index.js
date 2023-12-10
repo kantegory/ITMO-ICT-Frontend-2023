da52681f672366ac0a5ba3f98016183eb7abea9d
7Alexandra8 <alexp20030708@gmail.com>
2023-12-10 03:08:30 +0300

    Добавление файлов с домашней работой

---
+++ homeworks/K33392/Пронина Александра/hw5/pactice_5/vue-project/src/api/index.js
@@ -0,0 +1,10 @@
+import instance from "@/api/instance"
+import NotesApi from "@/api/notes"
+
+
+const notesApi = new NotesApi(instance)
+
+
+export {
+    notesApi
+}
---
+++ homeworks/K33392/Пронина Александра/hw5/pactice_5/vue-project/src/components/NoteCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <!-- отображение свойств в шаблоне можно сделать так: -->
+      <h5 class="card-title">{{ name }}</h5>
+      <p class="card-text">
+        {{ text }}
+      </p>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'NoteCard',
+
+
+  // свойства, которые принимает компонент
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: false
+    }
+  }
+}
+</script>
