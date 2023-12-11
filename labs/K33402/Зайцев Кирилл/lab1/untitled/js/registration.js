7d1eecf3ab119c00cea3f387d807315ffd264985
Kirill Zaitcev <lioishka@ya.ru>
2023-12-11 15:17:26 +0300

    Зайцев Кирилл К33402 ДЗ-2, ДЗ-3, ЛР-2

--- labs/K33402/Зайцев Кирилл/lab1/untitled/js/registration.js
+++
@@ -1,6 +0,0 @@
-// Обработчик события для формы регистрации
-document.getElementById('registrationForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    // Логика регистрации пользователя
-    // Здесь можно добавить код для отправки данных на сервер и создания нового аккаунта
-});
