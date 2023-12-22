f0a27a9552b980dc0c90204e56d65918409073e3
ko.ponomarev <ko.ponomarev@tinkoff.ru>
2023-12-23 00:25:47 +0300

    .feat: Страница регистрации

---
+++ labs/K33402/Ponomarev Konstantin Lab1/calories_calc/src/views/RegistrationPage.vue
@@ -0,0 +1,120 @@
+<script>
+import BaseLayout from "@/layouts/BaseLayout.vue";
+
+export default {
+  components: {BaseLayout},
+  data() {
+    return {
+      form: {
+        username: null,
+        password: null,
+        repeatPassword: null,
+        showLoading: false,
+        wasValidated: false,
+      }
+    }
+  },
+  methods: {
+    async submitForm() {
+      if (!this.formIsValid()) {
+        this.form.wasValidated = true
+      } else {
+        this.form.wasValidated = false
+        this.form.showLoading = true
+        //todo(отправка на сервер)
+      }
+    },
+    formIsValid() {
+      return this.form.password && !this.usernameError() && this.passwordMatch()
+    },
+    primaryButtonText() {
+      if (this.form.showLoading) {
+        return "Proccesing"
+      } else {
+        return "Create"
+      }
+    },
+    passwordMatch() {
+      return this.form.password === this.form.repeatPassword
+    },
+    usernameError() {
+      const usernamePattern = /^[a-zA-Z0-9_]+$/;
+      if (!this.form.username) {
+        return 'Please input your username';
+      } else if (!usernamePattern.test(this.form.username)) {
+        return 'Username can only have alphabetic symbols, numbers and underscores.';
+      }
+      return '';
+    }
+  },
+  computed: {
+    isCreateButtonDisabled() {
+      return this.form.showLoading
+    }
+  }
+};
+</script>
+
+<template>
+  <base-layout>
+    <h1>Create new account</h1>
+    <form class="mb-5 needs-validation"
+          :class="{ 'was-validated': form.wasValidated }"
+          ref="registrationForm"
+          @submit.prevent="submitForm"
+          novalidate>
+      <div class="mb-2">
+        <label for="registrationLogin" class="form-label">Username</label>
+        <div class="input-group has-validation">
+          <input
+              type="text"
+              id="registrationLogin"
+              class="form-control"
+              v-model="form.username"
+              required>
+          <div class="invalid-feedback">{{ usernameError() }}</div>
+        </div>
+      </div>
+
+      <div class="mb-2">
+        <label for="registrationPassword" class="form-label">Password</label>
+        <div class="input-group has-validation">
+          <input
+              type="password"
+              id="registrationPassword"
+              class="form-control"
+              v-model="form.password"
+              required>
+          <div class="invalid-feedback">Please input your password</div>
+        </div>
+      </div>
+
+      <div class="mb-4">
+        <label for="repeatRegistrationPassword" class="form-label">Repeat your password</label>
+        <div class="input-group has-validation">
+          <input
+              type="password"
+              id="repeatRegistrationPassword"
+              class="form-control"
+              v-model="form.repeatPassword"
+              required>
+          <div class="invalid-feedback">
+            The passwords do not match
+          </div>
+        </div>
+      </div>
+      <div>{{ form.wasValidated }}</div>
+      <button class="btn btn-primary w-100" type="submit" :disabled="form.showLoading">
+        <span class="spinner-border spinner-border-sm" aria-hidden="true"
+              :class="{'invisible': !form.showLoading}"></span>
+        <span role="status">{{ primaryButtonText() }}</span>
+      </button>
+    </form>
+  </base-layout>
+</template>
+
+<style scoped>
+h1 {
+  margin-bottom: 20px;
+}
+</style>
