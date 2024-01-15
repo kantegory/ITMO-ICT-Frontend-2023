cbd3379d449a3b4d3db046ee4003dd10f40d358b
ko.ponomarev <ko.ponomarev@tinkoff.ru>
2024-01-15 17:31:06 +0300

    .feat: Главная + добавил навбар

--- labs/K33402/Ponomarev Konstantin Lab1/calories_calc/src/App.vue
+++ labs/K33402/Ponomarev Konstantin Lab1/calories_calc/src/App.vue
@@ -1,3 +1,29 @@
 <template>
-  <router-view/>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">Калькулятор калорий</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Профиль
+              </a>
+              <ul class="dropdown-menu dropdown-menu-end">
+                <li><a class="dropdown-item" href="/calculatorParams">Настроить параметры калькулятора</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Выйти</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <router-view/>
+  </div>
 </template>
+<script setup>
+</script>
---
+++ labs/K33402/Ponomarev Konstantin Lab1/calories_calc/src/views/MainPage.vue
@@ -0,0 +1,49 @@
+<script setup>
+
+import BaseLayout from "@/layouts/BaseLayout.vue";
+</script>
+
+<template>
+  <base-layout>
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Ваш прогресс</h5>
+        <h6 class="card-subtitle mb-2 text-body-secondary">0/1000 ккал</h6>
+        <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="25" aria-valuemin="0"
+             aria-valuemax="100">
+          <div class="progress-bar" style="width: 25%"></div>
+        </div>
+        <div class="row mt-3 justify-content-between">
+          <div class="col">
+            <div>Белки</div>
+            <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="0" aria-valuemin="0"
+                 aria-valuemax="100">
+              <div class="progress-bar" style="width: 0%"></div>
+            </div>
+            <div>0/200 гр</div>
+          </div>
+          <div class="col">
+            <div>Жиры</div>
+            <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="0" aria-valuemin="0"
+                 aria-valuemax="100">
+              <div class="progress-bar" style="width: 0%"></div>
+            </div>
+            <div>0/200 гр</div>
+          </div>
+          <div class="col">
+            <div>Углеводы</div>
+            <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="0" aria-valuemin="0"
+                 aria-valuemax="100">
+              <div class="progress-bar" style="width: 0%"></div>
+            </div>
+            <div>0/200 гр</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </base-layout>
+</template>
+
+<style scoped>
+
+</style>
