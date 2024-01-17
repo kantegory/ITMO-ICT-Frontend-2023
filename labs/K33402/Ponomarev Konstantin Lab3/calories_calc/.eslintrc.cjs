459e8ca1cbafc0faaa8ce811a33985ee4de8ceba
ko.ponomarev <ko.ponomarev@tinkoff.ru>
2024-01-17 23:38:49 +0300

    Переименовал путь + отчет

---
+++ labs/K33402/Ponomarev Konstantin Lab3/calories_calc/.eslintrc.cjs
@@ -0,0 +1,15 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended'
+  ],
+  "env": {
+    "node": true,
+    "commonjs": true
+  },
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
+}
